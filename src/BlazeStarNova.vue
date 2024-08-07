e5ed9f9e98d78fac3535cb6cdefcc2a419a35ef6
Pat Udomprasert <pudompra@cfa.harvard.edu>
2024-08-07 19:20:11 -0400

    Add info text

--- src/BlazeStarNova.vue
+++ src/BlazeStarNova.vue
@@ -194,7 +194,42 @@
             <v-window-item>
               <v-card class="no-bottom-border-radius scrollable">
                 <v-card-text class="info-text no-bottom-border-radius">
-                  Information goes here
+                  <p>You may have heard that a star in our night sky is going to "go nova" soon. Learn what this means and how you can see the nova!</p>
+
+                  <h4>What is a nova?</h4>
+                  <p>
+                    Novas are bright stars that seem to appear out of nowhere. They are named for the Latin word for "new." In reality, stars that "go nova" have been there the whole time, but they are not very bright, and therefore not obvious in the sky. An explosion of gas on the surface of the star (the nova event) temporarily makes the star much, much brighter than it was before.
+                  </p>
+                  <h4>How do I see this nova?</h4>
+                  <p>
+                    The star that will become a nova is named "T Coronae Borealis." It is often referred to as "T CrB" for short and is also nicknamed, the "Blaze Star." As the name suggests, it is located within the constellation Corona Borealis, the "Northern Crown." This guide(TODO: link to the tour) explains how you can find Corona Borealis in your night sky.
+                  </p>
+                  <p>
+                    On a clear night, go out and look for Corona Borealis, so you can get used to its U-shape in the sky. Once T CrB goes nova, which can be any day now, or possibly weeks or months from now, it will seem as if a new star appeared just to the lower left of the U shape of the constellation.
+                  </p>
+                  <h4>How bright will the nova be?</h4>
+                  <p>
+                    At its normal brightness, T CrB is about a 10th magnitude star. This is about 30-40 times fainter than the faintest star a person could see from a dark sky, so you would need a telescope to see it. When it goes nova, it is predicted to be about 2-2.5 magnitudes. It is comparable in brightness to Alphecca, the brightest star you can see in the crown of Corona Borealis.(TODO: add a link to turn on the layer that shows the brightness comparison.)
+                  </p>
+                  <h4>What causes a nova?</h4>
+                  <p>
+                    Novas occur in binary star systems where small, very dense, very hot stars called white dwarfs orbit another large star at close range. The graviational pull from the white dwarf can move gas from the outer layers of the large companion star onto the surface of the white dwarf. When enough of this gas collects on the surface of the white dwarf, it triggers a nuclear explosion that causes the temporary brightening of the nova.
+                  </p>
+                  <h4>How do we know this nova is getting ready to blow?</h4>
+                  <p>
+                    Luckily, novas only blow up the gas that has settled on the surface of the white dwarf, and NOT the white dwarf itself. This means that the white dwarf can pull a new round of gas from its companion star. In some nova systems, this cycle of collecting gas, going nova, collecting more gas, going nova again, happens at a repeatable timescale. For T CrB, this has happened roughly every 80 years. It last blew in 1946, and before that, it blew in 1866. Astronomers have observed changes in the star's overall brightness that tell us it is getting ready to go nova again soon.
+                  </p>
+                  <h4>Why does this star have multiple names?</h4>
+                  <p>
+                    Astronomers usually name stars for the constellation they are in, and approximately for the relative brightness within the constellation, as designated by Greek letters. For example, the brightest star in Corona Borealis, Alphecca, is also known as &#945;-CrB. The 2nd brightest star is &#946;-CrB. 
+                  </p>
+                  <p>
+                    The astronomer Friedrich Argelander began the practice of assigning Roman letters to stars that vary in brightness, beginning with the letter R. So the "T" in T CrB means that it is the third variable star discovered within Corona Borealis.
+                  </p>
+                  <p>
+                    The nickname "Blaze Star" was given to T CrB after it "blazed forth suddenly" on May 12, 1866, becoming as bright as Alphecca. Learn more about the history in this 1897 <a href="https://articles.adsabs.harvard.edu/pdf/1897PA......5...97P" target="_blank"
+                    rel="noopener noreferrer">paper</a> by Mary Proctor.
+                  </p>
                 </v-card-text>
               </v-card>
             </v-window-item>
@@ -325,6 +360,7 @@ const showAltAzGrid = ref(true);
 const showControls = ref(false);
 const showConstellations = ref(true);
 const crbBelowHorizon = ref(true);
+const showTextSheet = ref(true);
 
 // For now, we're not allowing a user to change this
 const clockRate = 1000;
@@ -491,14 +527,14 @@ const cssVars = computed(() => {
   computed wrappers around modifying/querying that which can be used as
   dialog v-model values
 */
-const showTextSheet = computed({
-  get() {
-    return sheet.value === "text";
-  },
-  set(_value: boolean) {
-    selectSheet("text");
-  }
-});
+// const showTextSheet = computed({
+//   get() {
+//     return sheet.value === "text";
+//   },
+//   set(_value: boolean) {
+//     selectSheet("text");
+//   }
+// });
 
 const showVideoSheet = computed({
   get() {
@@ -648,6 +684,16 @@ body {
   overflow: hidden;
 
   font-family: Verdana, Arial, Helvetica, sans-serif;
+}
+
+h4 {
+  margin-top: 1rem;
+  margin-bottom: 0.5rem;
+  color: var(--accent-color);
+}
+
+p {
+  margin-bottom: 0.5rem;
 }
 
 #main-content {
