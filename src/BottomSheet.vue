7a6c82148a0d59a009f9a0a8742b9b35c9ef5185
John Arban Lewis <johnarban@users.noreply.github.com>
2024-08-09 10:29:31 -0400

    update figure caption

--- src/BottomSheet.vue
+++ src/BottomSheet.vue
@@ -32,19 +32,19 @@
               At its normal brightness, T CrB is about a 10th magnitude star. This is about 30-40 times fainter than the faintest star a person could see from a dark sky, so you would need a telescope to see it. When it goes nova, it is predicted to be about 2-2.5 magnitudes. It is comparable in brightness to Alphecca, the brightest star you can see in the crown of Corona Borealis.(TODO: add a link to turn on the layer that shows the brightness comparison.)
             </p>
             <h4>What causes a nova?</h4>
-            <v-row>
-              <v-col cols="7">
-                <p>
-                  Novas occur in binary star systems where small, very dense, very hot stars called white dwarfs orbit another large star at close range. The graviational pull from the white dwarf can pull gas from the outer layers of the large companion star onto the surface of the white dwarf. When enough of this gas collects on the surface of the white dwarf, it triggers a nuclear explosion that causes the temporary brightening of the nova.
-                </p>
-              </v-col>
-              <v-col cols="5">
-                <v-img
-                  src="https://www.nasa.gov/wp-content/uploads/2024/06/novacyg093500952-print.jpg"
-                ></v-img>
-                <p>TODO - this needs a caption with credit and alt text</p>
-              </v-col>
-            </v-row>
+            <p>
+              Novas occur in binary star systems where small, very dense, very hot stars called white dwarfs orbit another large star at close range. The graviational pull from the white dwarf can pull gas from the outer layers of the large companion star onto the surface of the white dwarf. When enough of this gas collects on the surface of the white dwarf, it triggers a nuclear explosion that causes the temporary brightening of the nova.
+            </p>
+            
+            <figure class="fig-right" style="width:40%;"> <!-- floats, the old magic, before flex-->
+              <v-img
+                src="https://www.nasa.gov/wp-content/uploads/2024/06/novacyg093500952-print.jpg"
+              ></v-img>
+              <figcaption>A red giant star and white dwarf orbit each other, similar to T Coronae Borealis, the Blaze Star. 
+                A disk of material is pulled off the red giant, into a disk, and then onto the white dwarf. <credit>(Credit: NASA / Goddard Space Flight Center)</credit>
+              </figcaption>
+            </figure>
+            
             <h4>How do we know this nova is getting ready to blow?</h4>
             <p>
               Luckily, novas only blow up the gas that has settled on the surface of the white dwarf, and NOT the white dwarf itself. This means that the white dwarf can pull a new round of gas from its companion star. In some nova systems, this cycle of collecting gas, going nova, collecting more gas, going nova again, happens at a repeatable timescale. For T CrB, this has happened roughly every 80 years. It last blew in 1946, and before that, it blew in 1866. Astronomers have observed changes in the star's overall brightness that suggest it is getting ready to go nova again soon!
@@ -239,5 +239,23 @@ watch(() => showTextSheet, (newVal) => {
   }
 }
 
+figure {
+  padding: 0.5em;
+  margin: 1em;
+  background-color: #151515;
+}
+
+.fig-right {
+  float: right;
+}
+
+.fig-left {
+  float: left;
+}
+
+figcaption {
+  font-size: 0.8em;
+  color: #ccc;
+}
 
 </style>
